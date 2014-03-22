62b97f60b709d6d48a0f50a50500ebcdaa7e0c90
Rajit Singh <rajit.b.singh@gmail.com>
2014-03-22 17:04:37 +0100

    Migrate from teddyhwang/bootstrap-datepicker to eternicode/bootstrap-datepicker

--- package.js
+++ package.js
@@ -5,5 +5,4 @@ Package.describe({
 Package.on_use(function (api, where) {
   api.add_files('lib/bootstrap-datepicker/js/bootstrap-datepicker.js', 'client');
   api.add_files('lib/bootstrap-datepicker/css/datepicker.css', 'client');
-  api.add_files('lib/bootstrap-datepicker/css/datepicker.css', 'client');
 });
