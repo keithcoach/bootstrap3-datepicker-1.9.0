444a506eeaff33206f00b812740b707517e8b7f7
Gerben van de Wiel <gerben.fds@gmail.com>
2014-04-27 16:11:06 +0200

    Ensure jquery is loaded before bootstrap3-datepicker

--- package.js
+++ package.js
@@ -3,6 +3,8 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
+  api.use('jquery', 'client');
+    
   api.add_files('lib/bootstrap-datepicker/js/bootstrap-datepicker.js', 'client');
   api.add_files('lib/bootstrap-datepicker/css/datepicker.css', 'client');
 });
