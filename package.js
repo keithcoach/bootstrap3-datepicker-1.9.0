9a5baa6ad1fa3495a65c59aaf59d1f5e461fc8cc
Rajit Singh <rajit.b.singh@gmail.com>
2015-02-25 13:02:58 +0100

    Depend on latest eternicode/bootstrap-datepicker

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "rajit:bootstrap3-datepicker",
   git: "https://github.com/rajit/bootstrap3-datepicker.git",
   summary: "Meteor packaging of eternicode/bootstrap-datepicker for Bootstrap 3",
-  "version": "1.3.1"
+  "version": "1.4.0"
 });
 
 Package.onUse(function (api) {
