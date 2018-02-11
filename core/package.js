ee485d48a0764c733f9f2472491b3fb19142c493
Rajit Singh <rajit@qeek.co>
2018-02-11 12:40:32 +0000

    Strip local node_modules/, update version to 1.7.1_1 and publish

--- core/package.js
+++ core/package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "rajit:bootstrap3-datepicker",
   git: "https://github.com/rajit/bootstrap3-datepicker.git",
   summary: "Meteor packaging of uxsolutions/bootstrap-datepicker for Bootstrap 3",
-  version: "1.7.1",
+  version: "1.7.1_1",
   documentation: 'README.md'
 });
 
